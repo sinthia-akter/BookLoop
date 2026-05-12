e8157f1b9ef265105e72a865a8e9e24427615c16
Samiha Tasnim <tasnimsamiha1174@gmail.com>
2026-05-12 19:57:14 +0600

    Fix database name: change bookloop_db_db to bookloop_db

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "bookloop_db_db";
+$database = "bookloop_db";
 
 // Create connection
 $conn = mysqli_connect($host, $username, $password, $database);
